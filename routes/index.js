3bbbcc9e584ee1f0cd16c6fdd6fa1afdb01ada1f
Michael Young <youngmd@iu.edu>
2018-11-07 12:45:11 -0500

    ADASS Tutorial Checkpoint 6

--- routes/index.js
+++ routes/index.js
@@ -15,14 +15,16 @@ router.get('/', function(req, res, next) {
 //   req:  All details of the client request
 //   res:  A response handler, used to return data/status/error to client
 //   next:  Call the next chained function if needed
-router.get('/stars', function(req, res, next) {
+
+// use router.post when you need your route to accept data
+router.post('/stars', function(req, res, next) {
     // Set the limit for number of results based on the url parameter
     // Set a default if limit is undefined
     var limit = req.query.limit !== undefined ? parseInt(req.query.limit) : 10;
 
     // Use the Star model to query the mongo stars collection
     db.Star
-        .find()
+        .find(req.body.query) //pass the POST data as the find parameters
         .limit(limit)
         .exec(function(err, _stars) {
             // if we encounter an error, call next() to let express error handling take care of it
